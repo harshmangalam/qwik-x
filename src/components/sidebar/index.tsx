2f1689a6833a8a67434cd0aeb8aef880b00520ca
harshmangalam <harshdev8218@gmail.com>
2023-08-10 00:11:28 +0530

    added notification icon

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -2,6 +2,7 @@ import { component$ } from "@builder.io/qwik";
 import { Logo } from "../logo";
 import { Link } from "@builder.io/qwik-city";
 import { HomeIcon, HomeOutlineIcon } from "~/icons/home";
+import { SearchIcon, SearchOutlineIcon } from "~/icons/search";
 
 export const Sidebar = component$(() => {
   const links = [
@@ -11,6 +12,12 @@ export const Sidebar = component$(() => {
       icon: HomeOutlineIcon,
       activeIcon: HomeIcon,
     },
+    {
+      name: "Search",
+      href: "/search/",
+      icon: SearchOutlineIcon,
+      activeIcon: SearchIcon,
+    },
   ];
   return (
     <aside>
@@ -19,7 +26,7 @@ export const Sidebar = component$(() => {
         {links.map(({ name, activeIcon: ActiveIcon, href, icon: Icon }) => (
           <li key={name}>
             <Link href={href} class="flex items-center gap-4">
-              <ActiveIcon />
+              <Icon />
               <span class="text-lg">Home</span>
             </Link>
           </li>
