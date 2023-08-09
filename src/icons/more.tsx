090e09741171fd7158788c2a33c3a831c326d0cb
harshmangalam <harshdev8218@gmail.com>
2023-08-10 00:31:49 +0530

    added more horiz icon

---
+++ src/icons/more.tsx
@@ -0,0 +1,12 @@
+import { component$ } from "@builder.io/qwik";
+import { Svg } from "~/components/svg";
+
+export const MoreHorizIcon = component$(() => {
+  return (
+    <Svg>
+      <g>
+        <path d="M3 12c0-1.1.9-2 2-2s2 .9 2 2-.9 2-2 2-2-.9-2-2zm9 2c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm7 0c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2z"></path>
+      </g>
+    </Svg>
+  );
+});
