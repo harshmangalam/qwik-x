fec0a4f6b68999d1329eeb9f57c3d1c422edc029
harshmangalam <harshdev8218@gmail.com>
2023-08-14 22:42:20 +0530

    added logo icon

--- src/routes/(auth)/login/index.tsx
+++ src/routes/(auth)/login/index.tsx
@@ -1,11 +1,18 @@
 import { component$ } from "@builder.io/qwik";
 import { LoginWithGithub } from "./login-with-github";
+import { LogoIcon } from "~/icons/logo";
 
 export default component$(() => {
   return (
     <div>
-      <article class="card card-bordered shadow max-w-xs w-full">
+      <article class="card card-bordered shadow max-w-md mx-auto w-full">
         <div class="card-body">
+          <div class="mb-6">
+            <div>
+              <LogoIcon />
+            </div>
+            <h2 class="text-3xl font-bold text-center">Sign in to X</h2>
+          </div>
           <LoginWithGithub />
         </div>
       </article>
