0ca3e3554c64f96f92f8a11eb2d98970517256b4
harshmangalam <harshdev8218@gmail.com>
2023-08-15 04:37:04 +0530

    Added postgres connection

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,9 +1,13 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+import { migrate } from "drizzle-orm/postgres-js/migrator";
 
-const client = createClient({
-  url: "DATABASE_URL",
-  authToken: "DATABASE_AUTH_TOKEN",
-});
-
+const connectionString = import.meta.env.DATABASE_URL;
+const client = postgres(connectionString);
 export const db = drizzle(client);
+
+// for migrations
+const migrationClient = postgres(connectionString, { max: 1 });
+await migrate(drizzle(migrationClient), {
+  migrationsFolder: "migrations",
+});
