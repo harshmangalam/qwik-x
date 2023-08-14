abd975c6a483391e9652b736a04e961b36152605
harshmangalam <harshdev8218@gmail.com>
2023-08-15 04:54:21 +0530

    added schema file

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,10 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
-
+import * as schema from "./schema";
 const connectionString = import.meta.env.DATABASE_URL;
 const client = postgres(connectionString);
-export const db = drizzle(client);
+export const db = drizzle(client, { schema });
 
 // for migrations
 const migrationClient = postgres(connectionString, { max: 1 });
