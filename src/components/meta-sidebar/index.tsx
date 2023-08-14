a013bcc287078c30195005e7437e9c2a27651194
harshmangalam <harshdev8218@gmail.com>
2023-08-14 16:09:44 +0530

    updated sidebar

--- src/components/meta-sidebar/index.tsx
+++ src/components/meta-sidebar/index.tsx
@@ -1,6 +1,7 @@
 import { component$ } from "@builder.io/qwik";
-import { UserItem } from "./user-item";
+
 import { Link } from "@builder.io/qwik-city";
+import { User } from "../user";
 
 export const MetaSidebar = component$(() => {
   return (
@@ -8,7 +9,7 @@ export const MetaSidebar = component$(() => {
       <ul class="menu bg-base-200 w-full rounded-box p-0">
         <li class="menu-title text-xl text-base-content py-4">Who to follow</li>
         {[...new Array(6)].map((_, i) => (
-          <UserItem key={i} />
+          <User key={i} />
         ))}
         <li>
           <Link
