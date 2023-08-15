e68a5773a54f7ab16bb4e10b10b6a22ce5ef3dfe
harshmangalam <harshdev8218@gmail.com>
2023-08-15 23:52:25 +0530

    mark userId as required column

--- src/database/schema/profile.ts
+++ src/database/schema/profile.ts
@@ -8,15 +8,21 @@ import {
   varchar,
 } from "drizzle-orm/pg-core";
 import { users } from "./users";
+import { type InferModel } from "drizzle-orm";
 
 export const profile = pgTable("profile", {
   id: serial("id").primaryKey(),
-  userId: integer("user_id").references(() => users.id),
+  userId: integer("user_id")
+    .references(() => users.id)
+    .notNull(),
   bio: varchar("bio", { length: 160 }),
-  avatar: json("avatar"),
+  avatar: json("avatar").notNull(),
   cover: json("cover"),
   category: text("category"),
   location: varchar("location", { length: 280 }),
   dob: timestamp("dob"),
   link: text("link"),
 });
+
+export type Profile = InferModel<typeof profile, "select">;
+export type NewProfile = InferModel<typeof profile, "insert">;
