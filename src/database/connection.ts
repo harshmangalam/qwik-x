be4e764a7db215d04f7b17495db7d903b2610adf
harshmangalam <harshdev8218@gmail.com>
2023-08-16 01:18:10 +0530

    updated pkgs

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,13 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 import * as schema from "./schema";
-const connectionString = import.meta.env.DATABASE_URL;
+
+const connectionString = process.env.DATABASE_URL as string;
 const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
-
-// for migrations
-const migrationClient = postgres(connectionString, { max: 1 });
-await migrate(drizzle(migrationClient), {
-  migrationsFolder: "migrations",
-});
