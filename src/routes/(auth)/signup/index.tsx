1a038b48256b7c175b21a70a420747db185af483
harshmangalam <harshdev8218@gmail.com>
2023-08-15 23:54:41 +0530

    added signup route action

--- src/routes/(auth)/signup/index.tsx
+++ src/routes/(auth)/signup/index.tsx
@@ -1,10 +1,26 @@
 import { component$ } from "@builder.io/qwik";
 import { LogoIcon } from "~/icons/logo";
-import { Form, Link } from "@builder.io/qwik-city";
+import { Form, Link, routeAction$, z, zod$ } from "@builder.io/qwik-city";
 import { TextInput } from "~/components/ui/text-input";
 import { Button } from "~/components/ui/button";
+import { handleSignup } from "~/utils/auth";
 
+export const useSignup = routeAction$(
+  (formData, requestEvent) => {
+    handleSignup(formData, requestEvent);
+  },
+  zod$({
+    name: z.string().nonempty("Enter value for name field"),
+    email: z
+      .string()
+      .nonempty("Enter value for email field")
+      .email("Enter valid email address"),
+    username: z.string().nonempty("Enter value for username field"),
+    password: z.string().nonempty("Enter value for password field"),
+  })
+);
 export default component$(() => {
+  const actionSig = useSignup();
   return (
     <div class="min-h-screen grid place-items-center">
       <article class="card card-bordered shadow max-w-md mx-auto w-full">
@@ -15,13 +31,37 @@ export default component$(() => {
             </Link>
             <h2 class="text-3xl font-bold text-center">Join qwik-X today</h2>
           </div>
-          <Form>
-            <TextInput label="Name" id="name" name="name" />
-            <TextInput label="Email" id="email" name="email" />
-            <TextInput label="Username" id="username" name="username" />
-            <TextInput label="Password" id="password" name="password" />
+          <Form action={actionSig}>
+            <TextInput
+              label="Name"
+              id="name"
+              name="name"
+              error={actionSig.value?.fieldErrors?.name?.[0]}
+            />
+            <TextInput
+              label="Email"
+              id="email"
+              name="email"
+              error={actionSig.value?.fieldErrors?.email?.[0]}
+            />
+            <TextInput
+              label="Username"
+              id="username"
+              name="username"
+              error={actionSig.value?.fieldErrors?.username?.[0]}
+            />
+            <TextInput
+              label="Password"
+              id="password"
+              name="password"
+              error={actionSig.value?.fieldErrors?.password?.[0]}
+            />
             <div class="mt-4">
-              <Button colorScheme="btn-primary" fullWidth>
+              <Button
+                loading={actionSig.isRunning}
+                colorScheme="btn-primary"
+                fullWidth
+              >
                 Sign up
               </Button>
             </div>
