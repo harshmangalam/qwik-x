eff16d67293a5761fc69e44c26d3f26675dbcc8a
harshmangalam <harshdev8218@gmail.com>
2023-08-16 23:40:13 +0530

    get current user

--- src/routes/(app)/layout.tsx
+++ src/routes/(app)/layout.tsx
@@ -1,7 +1,13 @@
 import { component$, Slot } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import { MetaSidebar } from "~/components/meta-sidebar";
 import { Sidebar } from "~/components/sidebar";
+import type { User } from "~/database/schema";
 
+export const useCurrentUser = routeLoader$(({ sharedMap }) => {
+  const user = sharedMap.get("user") as User;
+  return user;
+});
 export default component$(() => {
   return (
     <div class="relative container max-w-7xl mx-auto">
