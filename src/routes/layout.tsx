d0c62af205511f2045047f162d3b6d1a0e2e38a1
harshmangalam <harshdev8218@gmail.com>
2023-08-16 23:34:53 +0530

    added auth middleware

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -1,4 +1,11 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
+import { handleTokenVerification } from "~/utils/auth";
+
+export const onRequest: RequestHandler = async (requestEvent) => {
+  await handleTokenVerification(requestEvent);
+  await requestEvent.next();
+};
 
 export default component$(() => {
   return <Slot />;
