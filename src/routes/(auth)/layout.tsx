c42fed88a20279f02485991e0c65cae909790d96
harshmangalam <harshdev8218@gmail.com>
2023-08-16 23:35:15 +0530

    redirect loggedin user from auth page

--- src/routes/(auth)/layout.tsx
+++ src/routes/(auth)/layout.tsx
@@ -1,5 +1,10 @@
 import { Slot, component$ } from "@builder.io/qwik";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
+export const onRequest: RequestHandler = async ({ sharedMap, redirect }) => {
+  const user = sharedMap.get("user");
+  if (user) throw redirect(302, "/");
+};
 export default component$(() => {
   return (
     <div>
