c77e0a6644745c37cdac9b0fe389c2bf2100573b
harshmangalam <harshdev8218@gmail.com>
2023-08-17 12:59:42 +0530

    added signup schema

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -2,3 +2,10 @@ export type LoginSchema = {
   username: string;
   password: string;
 };
+
+export type SignupSchema = {
+  username: string;
+  password: string;
+  name: string;
+  email: string;
+};
