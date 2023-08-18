3417a46a2c47164e87308cae41960df2c87a5d9d
harshmangalam <harshdev8218@gmail.com>
2023-08-18 13:23:12 +0530

    added close icon

---
+++ src/icons/close.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+import { Svg } from "~/components/ui/svg";
+
+export const CloseIcon = component$(() => (
+  <Svg>
+    <g>
+      <path d="M10.59 12L4.54 5.96l1.42-1.42L12 10.59l6.04-6.05 1.42 1.42L13.41 12l6.05 6.04-1.42 1.42L12 13.41l-6.04 6.05-1.42-1.42L10.59 12z"></path>
+    </g>
+  </Svg>
+));
