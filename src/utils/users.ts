5719d7192fdd7503aad3d0c31491e4dffa0be123
harshmangalam <harshdev8218@gmail.com>
2023-08-25 12:48:26 +0530

    updated users suggestions

--- src/utils/users.ts
+++ src/utils/users.ts
@@ -101,8 +101,8 @@ async function fetchUsersSuggestion({ sharedMap }: RequestEventLoader) {
 async function fetchAllUserSuggestions({ sharedMap }: RequestEventLoader) {
   const user = sharedMap.get("user");
   const users = await db.query.users.findMany({
-    where(fields, { eq }) {
-      return not(eq(fields.id, user?.id));
+    where(fields, { eq, and }) {
+      return and(not(eq(fields.id, user?.id)), not);
     },
     columns: {
       id: true,
