85c603b6659b31863134617571d080e9c47477dc
harshmangalam <harshdev8218@gmail.com>
2023-08-29 09:20:31 +0530

    added comments schema

--- src/database/schema/posts.ts
+++ src/database/schema/posts.ts
@@ -12,6 +12,7 @@ import { users } from "./users";
 import { postsLikes } from "./posts-likes";
 // import { comments } from "./comments";
 import { bookmarks } from "./bookmarks";
+import { comments } from "./comments";
 
 export const visibilityEnum = pgEnum("post_visibility", ["Everyone", "Circle"]);
 export const replyPrivacyEnum = pgEnum("post_reply_privacy", [
@@ -42,9 +43,9 @@ export const postsRelations = relations(posts, ({ one, many }) => ({
     relationName: "authorToPosts",
   }),
   postsLikes: many(postsLikes),
-  // comments: many(comments, {
-  //   relationName: "postsToComments",
-  // }),
+  comments: many(comments, {
+    relationName: "postsToComments",
+  }),
   bookmarks: many(bookmarks, {
     relationName: "postsToBookmarks",
   }),
--- src/database/schema/users.ts
+++ src/database/schema/users.ts
@@ -14,6 +14,7 @@ import { postsLikes } from "./posts-likes";
 import { followers } from "./followers";
 import { followings } from "./followings";
 import { bookmarks } from "./bookmarks";
+import { comments } from "./comments";
 export const usersRole = pgEnum("users_role", ["User", "Admin"]);
 export const users = pgTable("users", {
   id: serial("id").primaryKey(),
@@ -42,6 +43,9 @@ export const usersRelations = relations(users, ({ one, many }) => ({
   }),
   followings: many(followings, {
     relationName: "usersToFollowings",
+  }),
+  comments: many(comments, {
+    relationName: "usersToComments",
   }),
   bookmarks: many(bookmarks, {
     relationName: "usersToBookmarks",
