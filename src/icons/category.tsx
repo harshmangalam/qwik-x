d8eca3d52425e7d91b10dbf8d986b6fd17435a1e
harshmangalam <harshdev8218@gmail.com>
2023-09-01 23:24:57 +0530

    added category icon

--- src/icons/category.tsx
+++ src/icons/category.tsx
@@ -3,10 +3,17 @@ import { Svg } from "~/components/ui/svg";
 
 export const EducationIcon = component$(() => {
   return (
-    <Svg>
-      <g>
-        <path d="M19.5 6H17V4.5C17 3.12 15.88 2 14.5 2h-5C8.12 2 7 3.12 7 4.5V6H4.5C3.12 6 2 7.12 2 8.5v10C2 19.88 3.12 21 4.5 21h15c1.38 0 2.5-1.12 2.5-2.5v-10C22 7.12 20.88 6 19.5 6zM9 4.5c0-.28.23-.5.5-.5h5c.28 0 .5.22.5.5V6H9V4.5zm11 14c0 .28-.22.5-.5.5h-15c-.27 0-.5-.22-.5-.5v-3.04c.59.35 1.27.54 2 .54h5v1h2v-1h5c.73 0 1.41-.19 2-.54v3.04zm0-6.49c0 1.1-.9 1.99-2 1.99h-5v-1h-2v1H6c-1.1 0-2-.9-2-2V8.5c0-.28.23-.5.5-.5h15c.28 0 .5.22.5.5v3.51z"></path>
-      </g>
+    <Svg fill="none" stroke="currentColor" stroke-width="2">
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M9.568 3H5.25A2.25 2.25 0 003 5.25v4.318c0 .597.237 1.17.659 1.591l9.581 9.581c.699.699 1.78.872 2.607.33a18.095 18.095 0 005.223-5.223c.542-.827.369-1.908-.33-2.607L11.16 3.66A2.25 2.25 0 009.568 3z"
+      ></path>
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M6 6h.008v.008H6V6z"
+      ></path>
     </Svg>
   );
 });
