5cad5aee98c548b663fcc73054dd8d54395c76bb
harshmangalam <harshdev8218@gmail.com>
2023-09-04 01:25:30 +0530

    added neon serverless

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,9 +1,8 @@
-import { sql } from "@vercel/postgres";
-import { drizzle } from "drizzle-orm/vercel-postgres";
 import * as schema from "./schema";
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
 
-console.log("connection.ts-POSTGRES_DATABASE:::", {
-  POSTGRES_DATABASE: process.env["POSTGRES_DATABASE"],
-});
+neonConfig.fetchConnectionCache = true;
 
+const sql = neon(process.env["DRIZZLE_DATABASE_URL"] as string);
 export const db = drizzle(sql, { schema });
