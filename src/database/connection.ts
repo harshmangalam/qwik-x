800fd2f49089fc9369ff450556591ad13745d523
harshmangalam <harshdev8218@gmail.com>
2023-09-04 23:34:23 +0530

    added neon postgres

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,12 @@
 import * as schema from "./schema";
-import postgres from "postgres";
-import { drizzle } from "drizzle-orm/postgres-js";
+// import postgres from "postgres";
+// import { drizzle } from "drizzle-orm/postgres-js";
 
-// import { neon, neonConfig } from "@neondatabase/serverless";
-// import { drizzle } from "drizzle-orm/neon-http";
-// neonConfig.fetchConnectionCache = true;
-// const sql = neon(process.env["DRIZZLE_DATABASE_URL"] as string);
-// export const db = drizzle(sql, { schema });
+import { neon, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+neonConfig.fetchConnectionCache = true;
+const sql = neon(process.env["DRIZZLE_DATABASE_URL"] as string);
+export const db = drizzle(sql, { schema });
 
-const client = postgres(process.env["DRIZZLE_DATABASE_URL"] as string);
-export const db = drizzle(client, { schema });
+// const client = postgres(process.env["DRIZZLE_DATABASE_URL"] as string);
+// export const db = drizzle(client, { schema });
