b4b6ef6da7f496cf23bf8e48d2bc09d4db90d16b
harshmangalam <harshdev8218@gmail.com>
2023-09-17 15:13:09 +0530

    updated connections

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,7 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
-
+import * as schema from "./schema";
 const DRIZZLE_DATABASE_URL = process.env["DRIZZLE_DATABASE_URL"] as string;
 
 if (!DRIZZLE_DATABASE_URL)
@@ -9,10 +9,10 @@ if (!DRIZZLE_DATABASE_URL)
 
 // for migrations
 const migrationClient = postgres(DRIZZLE_DATABASE_URL, { max: 1 });
-migrate(drizzle(migrationClient), {
+migrate(drizzle(migrationClient, { schema }), {
   migrationsFolder: "migrations",
 });
 
 // for query purposes
 const queryClient = postgres(DRIZZLE_DATABASE_URL);
-export const db = drizzle(queryClient);
+export const db = drizzle(queryClient, { schema });
