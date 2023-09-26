afd621c0d64833e9e05496cc75e7607ee1db068c
harshmangalam <harshdev8218@gmail.com>
2023-09-27 02:49:39 +0530

    fix migration issues

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,17 +1,10 @@
 import { drizzle } from "drizzle-orm/postgres-js";
-import { migrate } from "drizzle-orm/postgres-js/migrator";
 import postgres from "postgres";
 import * as schema from "./schema";
 const DRIZZLE_DATABASE_URL = process.env["DRIZZLE_DATABASE_URL"] as string;
 
 if (!DRIZZLE_DATABASE_URL)
   throw new Error("DRIZZLE_DATABASE_URL env variable missing");
-
-// for migrations
-const migrationClient = postgres(DRIZZLE_DATABASE_URL, { max: 1 });
-migrate(drizzle(migrationClient, { schema }), {
-  migrationsFolder: "migrations",
-});
 
 // for query purposes
 const queryClient = postgres(DRIZZLE_DATABASE_URL);
