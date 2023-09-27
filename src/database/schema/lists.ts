7d7a52a9116a72959c47a573264f22e49a4bc4b3
harshmangalam <harshdev8218@gmail.com>
2023-09-27 12:00:27 +0530

    updated lists schema

--- src/database/schema/lists.ts
+++ src/database/schema/lists.ts
@@ -12,7 +12,7 @@ import { users } from "./users";
 export const lists = pgTable("lists", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 25 }).notNull(),
-  description: varchar("description", { length: 280 }),
+  description: varchar("description", { length: 100 }),
   isPrivate: boolean("is_private").default(false).notNull(),
   ownerId: integer("owner_id").notNull(),
   createdAt: timestamp("created_at", { withTimezone: true })
