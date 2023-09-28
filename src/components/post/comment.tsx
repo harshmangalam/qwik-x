38b7cac4888ca73507f53a221130f7e0b4d55e7b
harshmangalam <harshdev8218@gmail.com>
2023-09-28 12:52:20 +0530

    changed post actions styles

--- src/components/post/comment.tsx
+++ src/components/post/comment.tsx
@@ -1,24 +1,30 @@
 import { $, type QwikMouseEvent, component$ } from "@builder.io/qwik";
 import { useNavigate } from "@builder.io/qwik-city";
 import { CommentOutlineIcon } from "~/icons/comment";
+import { Button } from "../ui/button";
 
-type Props = { postId: number; count?: number };
-export const Comment = component$(({ count, postId }: Props) => {
-  const navigate = useNavigate();
-  const handleComment = $((ev: QwikMouseEvent) => {
-    ev.stopPropagation();
-    navigate(`/posts/${postId}`);
-  });
-  return (
-    <div>
-      <button
-        preventdefault:click
-        onClick$={handleComment}
-        class="btn btn-ghost"
-      >
-        <CommentOutlineIcon />
-        {count}
-      </button>
-    </div>
-  );
-});
+type Props = { postId: number; count?: number; isLarge?: boolean };
+export const Comment = component$(
+  ({ count, postId, isLarge = false }: Props) => {
+    const navigate = useNavigate();
+    const handleComment = $((ev: QwikMouseEvent) => {
+      ev.stopPropagation();
+      navigate(`/posts/${postId}`);
+    });
+    return (
+      <div class="flex items-center group gap-1">
+        <Button
+          size={isLarge ? "btn-md" : "btn-sm"}
+          circle
+          btnClass={"group-hover:btn-info"}
+          preventdefault:click
+          onClick$={handleComment}
+          colorScheme="btn-ghost"
+        >
+          <CommentOutlineIcon />
+        </Button>
+        <div class={["group-hover:text-info"]}>{count}</div>
+      </div>
+    );
+  }
+);
--- src/components/post/share.tsx
+++ src/components/post/share.tsx
@@ -1,10 +1,22 @@
 import { component$ } from "@builder.io/qwik";
 import { ShareOutlineIcon } from "~/icons/share";
+import { Button } from "../ui/button";
 
-export const Share = component$(() => {
-  return (
-    <button class="btn btn-ghost">
-      <ShareOutlineIcon />
-    </button>
-  );
-});
+export const Share = component$(
+  ({ isLarge = false }: { isLarge?: boolean }) => {
+    return (
+      <div class="group">
+        <Button
+          preventdefault:click
+          onClick$={() => {}}
+          colorScheme="btn-ghost"
+          size={isLarge ? "btn-md" : "btn-sm"}
+          circle
+          btnClass={"group-hover:btn-accent"}
+        >
+          <ShareOutlineIcon />
+        </Button>
+      </div>
+    );
+  }
+);
