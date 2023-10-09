a2dec1c4f173fc41ac5152286ffd67473bafb4cc
harshmangalam <harshdev8218@gmail.com>
2023-10-09 21:34:06 +0530

    update theme on load

--- src/hooks/useThemes.tsx
+++ src/hooks/useThemes.tsx
@@ -1,8 +1,9 @@
 import { $, useSignal, useVisibleTask$ } from "@builder.io/qwik";
+import { useCurrentUser } from "~/routes/(app)/layout";
 
 export const useThemes = () => {
+  const currentUser = useCurrentUser();
   const theme = useSignal("");
-
   useVisibleTask$(({ track }) => {
     track(() => theme.value);
     const html = document.querySelector("html");
@@ -10,6 +11,15 @@ export const useThemes = () => {
     if (currentTheme) {
       theme.value = currentTheme;
     }
+  });
+
+  useVisibleTask$(({ track }) => {
+    track(() => currentUser.value);
+    const html = document.querySelector("html");
+    const value = currentUser.value?.theme as any;
+    const colorScheme = value?.colorScheme;
+    html?.setAttribute("data-theme", colorScheme);
+    theme.value = colorScheme;
   });
 
   const updateTheme = $((value: string) => {
