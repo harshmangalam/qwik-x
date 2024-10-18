749f876c207f60e48c711575aadbf114d0a82a61
harshmangalam <harshdev8218@gmail.com>
2024-10-18 12:20:37 +0530

    fix alert component issues

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,42 +1,37 @@
-import { component$, useStore } from "@builder.io/qwik";
+import { $, component$, useStore } from "@builder.io/qwik";
 
 type Props = {
   text: string;
   status: "alert-info" | "alert-success" | "alert-warning" | "alert-error";
-  onClose?: () => void;
 };
 
 export const Alert = component$((props: Props) => {
-  const { text, status = "alert-info", onClose } = props;
+  const { text, status = "alert-info" } = props;
   const store = useStore({ isVisible: true });
 
   const handleClose = $(() => {
     store.isVisible = false;
-    if (onClose) {
-      onClose();
-    }
   });
 
   if (!store.isVisible) return null;
 
   return (
-    <div 
-      role="alert" 
-      aria-live="assertive" 
-      aria-atomic="true" 
-      class={["alert", status]} 
+    <div
+      role="alert"
+      aria-live="assertive"
+      aria-atomic="true"
+      class={["alert flex justify-between gap-2", status]}
       aria-label="Alert"
     >
       <span>{text}</span>
-      {onClose && (
-        <button 
-          onClick$={handleClose} 
-          aria-label="Close alert"
-          class="close-button"
-        >
-          &times; {/* Close icon */}
-        </button>
-      )}
+
+      <button
+        onClick$={handleClose}
+        aria-label="Close alert"
+        class="close-button"
+      >
+        &times; {/* Close icon */}
+      </button>
     </div>
   );
 });
